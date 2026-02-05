cbf74067a946d7ebb3abf14f8b2c6812d33a0131
purilin <linhe6982@gmail.com>
2026-02-05 05:29:22 +0000

    重构插件管理

--- src/plugin.rs
+++ src/plugin.rs
@@ -1,13 +1,14 @@
 use crate::{
     core::event::EventNexus,
-    prelude::{ActionManager, Message},
-    types::plugin_type::{BasePlugin, Plugin, Trigger},
+    prelude::{ActionManager, Message, PrivateMessageEvent},
+    types::plugin_type::{BasePlugin, PluginWrapper},
 };
+use async_trait::async_trait;
 use std::sync::Arc;
 use tokio::sync::RwLock;
 
 pub struct PluginManager {
-    plugins: RwLock<Vec<Arc<Plugin>>>,
+    plugins: Arc<RwLock<Vec<Arc<PluginWrapper>>>>,
     act: Arc<ActionManager>,
     event_nexus: Arc<EventNexus>,
 }
@@ -15,7 +16,7 @@ pub struct PluginManager {
 impl PluginManager {
     pub fn new(act: Arc<ActionManager>, event_nexus: Arc<EventNexus>) -> Arc<Self> {
         Arc::new(PluginManager {
-            plugins: RwLock::new(Vec::new()),
+            plugins: Arc::new(RwLock::new(Vec::new())),
             act,
             event_nexus,
         })
@@ -26,60 +27,60 @@ impl PluginManager {
             "[插件加载] [数量: {}] 加载中...",
             self.plugins.read().await.len()
         );
-        let plugins_arc = self.plugins.read().await.clone();
-        for plugin in plugins_arc {
-            let arc_plugin = plugin.clone();
-            let event_nexus = self.event_nexus.clone();
-            let act = self.act.clone();
-            tokio::spawn(async move {
-                arc_plugin
-                    .clone()
-                    .on_load(event_nexus.clone(), act.clone())
-                    .await;
-                arc_plugin
-                    .clone()
-                    .on_update(event_nexus.clone(), act.clone())
-                    .await;
-            });
+        let plugins = self.plugins.read().await.clone();
+        for plugin in plugins {
+            let plugin = plugin.clone();
+            plugin.clone().on_plugin_load().await;
+            tokio::spawn(plugin.run(self.event_nexus.clone(), self.act.clone()));
         }
     }
 
-    pub async fn add_plugin(self: Arc<Self>, plugin: Plugin) {
+    pub async fn add_plugin(self: Arc<Self>, plugin: PluginWrapper) {
         let arc_self = self.clone();
         arc_self.plugins.write().await.push(Arc::new(plugin));
     }
 
-    async fn get_plugin_info(self: Arc<Self>) -> String {
-        let plugins = self.clone().plugins.read().await.clone();
-
-        let mut help_str = String::from(">插件列表\n");
-        for plugin in plugins {
-            help_str.push_str(&format!("{}\n\n", plugin.get_info_str()));
-        }
-        help_str = help_str.trim().to_string();
-        help_str.push_str("\n--->--->");
-        help_str
-    }
-
     pub async fn run(self: Arc<Self>) {
-        let self_for_help_plugin = self.clone();
-        let help_plugin = Plugin::new()
-            .with_name("Get Help")
-            .with_author("purilin")
-            .with_description("/help")
-            .with_trigger(Trigger::StartWith("/help".to_string()))
-            .with_on_private_message_func(move |msg, act| {
-                let cloned_self = self_for_help_plugin.clone();
-                async move {
-                    let help_info_cloned = cloned_self.clone().get_plugin_info().await;
-                    act.send_private_message(
-                        msg.sender.user_id,
-                        Message::new().with_text(help_info_cloned),
-                    )
-                    .await;
-                }
-            });
+        let help_plugin = PluginWrapper::new(HelpPlugin::new(self.plugins.clone()))
+            .with_name("GetHelpList")
+            .with_description("/help");
         self.clone().add_plugin(help_plugin).await;
         tokio::spawn(self.clone().handle_plugin());
     }
+}
+
+struct HelpPlugin {
+    plugins: Arc<RwLock<Vec<Arc<PluginWrapper>>>>,
+}
+
+impl HelpPlugin {
+    pub fn new(plugins: Arc<RwLock<Vec<Arc<PluginWrapper>>>>) -> Self {
+        Self { plugins }
+    }
+
+    async fn on_private_message(&self, msg: Arc<PrivateMessageEvent>, act: Arc<ActionManager>) {
+        if msg.raw_message.starts_with("/help") {
+            let mut info = String::from("[PluginList]\n");
+            for plugin in self.plugins.read().await.iter() {
+                info.push_str(&format!("{}\n\n", plugin.get_info_str()));
+            }
+            let info = info.trim().to_string();
+            act.send_private_message(msg.sender.user_id, Message::new().with_text(info))
+                .await;
+        }
+    }
+}
+
+#[async_trait]
+impl BasePlugin for HelpPlugin {
+    async fn on_load(self: Arc<Self>) {}
+    async fn on_update(self: Arc<Self>, event_nexus: Arc<EventNexus>, act: Arc<ActionManager>) {
+        let private_port = event_nexus.get_private_message_port();
+        let Ok(private_message) = private_port.recv().await else {
+            return;
+        };
+        self.on_private_message(private_message.clone(), act.clone())
+            .await;
+    }
+    async fn on_unload(self: Arc<Self>) {}
 }
--- src/prelude.rs
+++ src/prelude.rs
@@ -6,6 +6,6 @@ pub use crate::{
         action_type::NapcatRequestData,
         event_type::message_event::{GroupMessageEvent, PrivateMessageEvent},
         message_type::Message,
-        plugin_type::{Plugin, Trigger},
+        plugin_type::{BasePlugin, PluginWrapper},
     },
 };
